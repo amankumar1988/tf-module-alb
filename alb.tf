9a44c077190bb06e1b3d67cfd4f52a0dce69c199
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-29 08:04:11 +0530

    Auto Push

--- alb.tf
+++ alb.tf
@@ -3,18 +3,9 @@ resource "aws_lb" "alb" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
-
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
-
+  security_groups    = var.INTERNAL ? [aws_security_group.alb_private.id] : [aws_security_group.alb_public.id]
+  subnets            = var.INTERNAL ? [data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS] : [data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS]
   tags = {
-    Environment = "production"
+    Name             = var.ALB_NAME
   }
 }
